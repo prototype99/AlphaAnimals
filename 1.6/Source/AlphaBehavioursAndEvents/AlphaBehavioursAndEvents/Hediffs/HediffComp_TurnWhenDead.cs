3d91f79d6e2c728fec99ace9ee129da7b0f0b6ba
Seirra <sophietheopossum@yandex.ru>
2025-11-06 01:50:36 +0000

    Refactor hediff comp turn logic
    Streamlines the `HediffComp_TurnWhenDead` logic by improving null and condition checks, utilizing the null-conditional operator.
    
    Simplifies method calls by removing redundant arguments for `TargetInfo`, `TryStartMentalState`, and `TraverseParms.For`. Converts the `Props` property to an expression-bodied member for conciseness and removes an unused `System` using.

--- 1.6/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/Hediffs/HediffComp_TurnWhenDead.cs
+++ 1.6/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/Hediffs/HediffComp_TurnWhenDead.cs
@@ -1,7 +1,6 @@
 ﻿using RimWorld;
 using Verse;
 using Verse.Sound;
-using System;
 
 namespace AlphaBehavioursAndEvents
 {
@@ -9,61 +8,48 @@ namespace AlphaBehavioursAndEvents
     {
        
 
-        public HediffCompProperties_TurnWhenDead Props
-        {
-            get
-            {
-                return (HediffCompProperties_TurnWhenDead)this.props;
-            }
-        }
+        public HediffCompProperties_TurnWhenDead Props => (HediffCompProperties_TurnWhenDead)props;
 
         public override void Notify_PawnDied(DamageInfo? dinfo, Hediff culprit = null)
         {
             //base.Notify_PawnDied();
             float severityToTurn = Props.severityToTurn;
 
-            if (this.parent.pawn.Corpse == null)
+            Map map = parent.pawn.Corpse?.Map;
+            if (map == null || !(parent.Severity > severityToTurn)) return;
+            Gender oldGender = parent.pawn.gender;
+            Faction faction = null;
+            if (Props.isHostile)
             {
-                return;
+                faction = Find.FactionManager.FirstFactionOfDef(FactionDefOf.AncientsHostile);
             }
-            Map map = this.parent.pawn.Corpse.Map;
-            if (map != null && this.parent.Severity> severityToTurn) {
-                Gender oldGender = this.parent.pawn.gender;
-                Faction faction = null;
+            int numToSpawn = Rand.RangeInclusive(Props.numberOfSpawn[0], Props.numberOfSpawn[1]);
+            for (int i = 0; i < numToSpawn; i++) {
+                PawnGenerationRequest request = new PawnGenerationRequest(PawnKindDef.Named(Props.thingToTurnTo), faction, PawnGenerationContext.NonPlayer, -1, false, true, false, false, true,  1f, false, true, true, false, false);
+                Pawn pawn = PawnGenerator.GeneratePawn(request);
+                PawnUtility.TrySpawnHatchedOrBornPawn(pawn, parent.pawn.Corpse);
+                if (Props.keepGender)
+                {
+                    pawn.gender = oldGender;
+                }
                 if (Props.isHostile)
                 {
-                     faction = Find.FactionManager.FirstFactionOfDef(FactionDefOf.AncientsHostile);
+                    pawn.mindState.mentalStateHandler.TryStartMentalState(MentalStateDefOf.ManhunterPermanent, null, true);
                 }
-                int numToSpawn = Rand.RangeInclusive(Props.numberOfSpawn[0], Props.numberOfSpawn[1]);
-                for (int i = 0; i < numToSpawn; i++) {
-                    PawnGenerationRequest request = new PawnGenerationRequest(PawnKindDef.Named(Props.thingToTurnTo), faction, PawnGenerationContext.NonPlayer, -1, false, true, false, false, true,  1f, false, true, true, false, false);
-                    Pawn pawn = PawnGenerator.GeneratePawn(request);
-                    PawnUtility.TrySpawnHatchedOrBornPawn(pawn, this.parent.pawn.Corpse);
-                    if (Props.keepGender)
-                    {
-                        pawn.gender = oldGender;
-                    }
-                    if (Props.isHostile)
-                    {
-                        pawn.mindState.mentalStateHandler.TryStartMentalState(MentalStateDefOf.ManhunterPermanent, null, true, false, false, null, false);
-                    }
-
-                }
-
-                for (int i = 0; i < 20; i++)
-                {
-                    IntVec3 c;
-                    CellFinder.TryFindRandomReachableCellNearPosition(this.parent.pawn.Corpse.Position, this.parent.pawn.Corpse.Position, map, 2, TraverseParms.For(TraverseMode.NoPassClosedDoors, Danger.Deadly, false), null, null, out c);
-                   
-                    FilthMaker.TryMakeFilth(c, this.parent.pawn.Corpse.Map, ThingDefOf.Filth_Blood);
-                    
-                }
-                
-               
-                InternalDefOf.Hive_Spawn.PlayOneShot(new TargetInfo(this.parent.pawn.Corpse.Position, map, false));
-                this.parent.pawn.Corpse.Destroy();
 
             }
+
+            for (int i = 0; i < 20; i++)
+            {
+                CellFinder.TryFindRandomReachableCellNearPosition(parent.pawn.Corpse.Position, parent.pawn.Corpse.Position, map, 2, TraverseParms.For(TraverseMode.NoPassClosedDoors), null, null, out IntVec3 c);
+                   
+                FilthMaker.TryMakeFilth(c, parent.pawn.Corpse.Map, ThingDefOf.Filth_Blood);
+                    
+            }
+                
+               
+            InternalDefOf.Hive_Spawn.PlayOneShot(new TargetInfo(parent.pawn.Corpse.Position, map));
+            parent.pawn.Corpse.Destroy();
 
         }
 
